fda98cf28325311e5495ca95c4609bf55fad49b2
Ahzreia <ahzreia@gmail.com>
2018-04-11 20:08:21 -0400

    Built makeGrid function

--- Lesson 21/designs.js
+++ Lesson 21/designs.js
@@ -1,10 +1,27 @@
+
+
 // Select color input
+
+
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+// Functions for the group
 
-function makeGrid() {
+function makeGrid() {   //builds grid
 
-// Your code goes here!
+      //set dimensions
+      var rows = $('#inputHeight').val();
+      var columns = $('#inputWidth').val();
 
+      canvas.empty() //empty previous grid
+
+      for (x = 0; x < rows; x++) { //create rows
+        canvas.append('<tr></tr>');
+      }
+
+      lines = $('tr');
+      for (y = 0; y < columns; y++) { //create columns
+        lines.append('<td></td>');
+      }
 }
+// When the size is submitted by the user, call makeGrid()
