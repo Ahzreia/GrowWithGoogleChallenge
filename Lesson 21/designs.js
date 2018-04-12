195be100bb09fa16386cbca1961a1af77d654e7e
Ahzreia <ahzreia@gmail.com>
2018-04-11 22:55:28 -0400

    Created Grid

--- Lesson 21/designs.js
+++ Lesson 21/designs.js
@@ -1,10 +1,29 @@
+//set constants
+
+
 // Select color input
+
+
 // Select size input
 
+$('input[type="submit"]').on('click', function(event){
+  event.preventDefault();
+  let columns = $('#inputWidth').val();
+  let rows = $('#inputHeight').val();
+  $('#pixelCanvas').empty();
+  makeGrid(columns, rows);
+});
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
 
-// Your code goes here!
+//function to makeGrid
+function makeGrid(columns, rows) {
 
+  for (x = 0; x < rows; x++) {  //create rows
+        $('#pixelCanvas').append($("<tr></tr>"));
+
+    for (let y = 0; y < columns; y++) { //create columns
+      $('tr').last().append($("<td></td>"));
+    }
+  }
 }
