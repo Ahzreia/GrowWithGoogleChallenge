2a73b4c92507c5678d5a5850e473cde87183dc07
Ahzreia <ahzreia@gmail.com>
2018-04-11 21:46:33 -0400

    Reset

--- Lesson 21/designs.js
+++ Lesson 21/designs.js
@@ -1,27 +1,10 @@
-
-
 // Select color input
-
-
 // Select size input
 
-// Functions for the group
+// When size is submitted by the user, call makeGrid()
 
-function makeGrid() {   //builds grid
+function makeGrid() {
 
-      //set dimensions
-      var rows = $('#inputHeight').val();
-      var columns = $('#inputWidth').val();
+// Your code goes here!
 
-      canvas.empty() //empty previous grid
-
-      for (x = 0; x < rows; x++) { //create rows
-        canvas.append('<tr></tr>');
-      }
-
-      lines = $('tr');
-      for (y = 0; y < columns; y++) { //create columns
-        lines.append('<td></td>');
-      }
 }
-// When the size is submitted by the user, call makeGrid()
